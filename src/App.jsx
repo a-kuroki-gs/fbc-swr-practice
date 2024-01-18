cd3a1d4152a4c26178acce75e05a0c903866f236
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-01-18 13:53:40 +0900

    コンポーネント外で定義していたfetcherをコンポーネント内に移動
    コンポーネント外で定義する必要がなかったため
    記述も複雑になっていたため

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import useSWR from "swr";
 import "./App.css";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR([url, headers], ([url, headers]) =>
     fetcher(url, { headers })
