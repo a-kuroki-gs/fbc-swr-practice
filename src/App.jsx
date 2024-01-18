6c65cdd55cad3fccc371dc131eaab96a916a3deb
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-01-18 14:07:27 +0900

    useSWRの引数からheadersを取り除く
    urlは引数で指定すべきだが、headersはリソース毎に切り替えることはまれなため
    urlのみ指定した方がシンプルに書けるため

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import "./App.css";
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR([url, headers], ([url, headers]) =>
-    fetcher(url, { headers })
-  );
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
   if (error) return <p>Failed to load.</p>;
   if (isLoading) return <p>Loading...</p>;
